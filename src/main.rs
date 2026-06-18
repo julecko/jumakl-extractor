14f6244d97a9e502da73ce15982f64c1fe52c7d5
Julecko <julecko@dilino.sk>
2026-06-18 13:13:27 +0200

    Started learning how to work with rust requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url: &str = "http://example.com";
+
+    let response = match reqwest::get(url).await {
+        Ok(resp) => resp,
+        Err(err) => {
+            eprintln!("Request failed: {err}");
+            return Err(Box::<dyn std::error::Error>::from(err));
+        }
+    };
+
+    println!("Status: {}", response.status());
+    let body: String = match response.text().await {
+        Ok(body) => body,
+        Err(err) => {
+            eprintln!("Getting request content failed");
+            return Err(Box::<dyn std::error::Error>::from(err));
+        }
+    };
+
+    println!("Content:");
+    println!("{body}");
+
+
+    return Ok(());
 }
